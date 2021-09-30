0b3c659acec5bc4128497bf8c80ebad2b86981f8
Narimon2001 <naasengsuriya123@gmail.com>
2021-09-30 10:50:11 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan Kedua</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <?php 
+    $nama = 'NARIMON EEN';
+    $umur = 18 + 2;
+
+    echo "$nama berumur $umur tahun";
+    ?>
+
+</body>
+</html>
